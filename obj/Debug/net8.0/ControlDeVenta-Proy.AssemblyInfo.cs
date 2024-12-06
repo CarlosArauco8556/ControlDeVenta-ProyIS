279131071b3a77693bc5f21b86646f391095869b
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-06 16:26:52 -0300

    chore: intall packages and nuggets.

--- obj/Debug/net8.0/ControlDeVenta-Proy.AssemblyInfo.cs
+++ obj/Debug/net8.0/ControlDeVenta-Proy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlDeVenta-Proy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1b72b4ea29b038cbd17378a169a119ebe36760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86672d1aae50f590d03bf45b6d32595b2e498453")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlDeVenta-Proy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlDeVenta-Proy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
