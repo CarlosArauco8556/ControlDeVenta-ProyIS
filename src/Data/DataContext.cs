e387a658a1bdd864785d135b7389dee571fea5f3
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-06 23:23:14 -0300

    chore: nuggets and identity dependencies are installed

---
+++ src/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControlDeVenta_Proy.src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlDeVenta_Proy.src.Data
+{
+    public class DataContext 
+    {
+
+    }
+}
---
+++ src/Data/DataSeeder.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlDeVenta_Proy.src.Data
+{
+    public class DataSeeder
+    {
+        
+    }
+}
