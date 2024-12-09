c246990a13cf1ddffdad129a057ac6affe15e516
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-09 17:28:35 -0300

    feat: add supplier interface.

--- src/Interfaces/ISupplierRepository.cs
+++ src/Interfaces/ISupplierRepository.cs
@@ -1,8 +1,13 @@
+using ControlDeVenta_Proy.src.DTOs;
+using ControlDeVenta_Proy.src.Models;
 
 namespace ControlDeVenta_Proy.src.Interfaces
 {
     public interface ISupplierRepository
     {
-        
+        Task<IEnumerable<NewSupplierDto>> GetSuppliers();
+        Task<NewSupplierDto> AddSupplier(Supplier supplier);
+        Task<NewSupplierDto> UpdateSupplier(Supplier supplier);
+        Task<NewSupplierDto> DeleteSupplier(int id);
     }
 }
